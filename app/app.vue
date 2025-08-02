97804f0db4f96fd3e4a4d1b74c554379ca60a2e9
D3Portillo <d3portillo@gmail.com>
2025-08-02 08:59:11 +0200

    chores: add tailwindcss

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtRouteAnnouncer />
+  <main class="p-4">
+    <h2 class="font-bold text-3xl text-black">World Starter Template</h2>
+    <p>A Nuxt flavored start pack for building Mini Apps for Worldchain</p>
+    <button class="bg-blue-500 mt-4 text-white font-bold py-2 px-4 rounded-lg">
+      Connect Wallet
+    </button>
+  </main>
 </template>
